04a7bad142a407bfafa1d7f355dd0cfacf5073f8
MSEIKAM <mariemas2002@gmail.com>
2024-12-02 17:04:46 +0100

    Fin-AriAuthBundle

---
+++ src/AriAuthBundle.php
@@ -0,0 +1,9 @@
+<?php
+// src/AriAuthBundle.php
+namespace AriAuthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AriAuthBundle extends Bundle
+{
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace AriAuthBundle;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
